4a171418c45ec0e601b5fa5e27497e2d629b20a3
herablog <kz.route21@gmail.com>
2013-01-24 23:33:03 +0900

    Change DOM.

--- public/js/modules/master.js
+++ public/js/modules/master.js
@@ -8,9 +8,7 @@ var socket = io.connect(config.ws);
 
 // connect
 socket.on('connect', function(c){
-	console.log('connect', c)
-	// socket.emit('changePage', parseInt(self.state.params[0]) - 1);
-
+	
 });
 
 // disconnect
@@ -19,26 +17,57 @@ socket.on('disconnect', function(data) {
 });
 
 // get user data
-socket.on('user', function(data){
-	var target = $('#list').find('.' + data.id);
+socket.on('user', function(data) {	
+	var selector = 'li[data-id="' + data.id + '"]';
+	var target = $('#list').find(selector);
 	if (target.length()) {
-		target.data('name', data.name).text(data.name + data.message)
+		target.empty();
+		target
+			.data({ name: data.name, id: data.id })
+			.tag('p.name').text(data.name).gat()
+			.tag('p.message').text(data.message).gat();
 	} else {
 		$('#list').append(
-			$.tag('li').cls(data.id).data('name', data.name).text(data.name + data.message).tap(
-				function() {
-					location.replace('/master/' + data.id)
-				}
-			)
+			$.tag('li')
+				.data({ name: data.name, id: data.id })
+				.text(data.name + data.message)
+				.tap(
+					function() {
+						location.replace('/master/' + data.id)
+					}
+				)
+				.tag('p.name').text(data.name).gat()
+				.tag('p.message').text(data.message).gat()
 		);
 	}
 });
 
-socket.on('data', function(data){
-	var target = $('#contents').find('#user');
-	if (target.length()) {
-		
-	}
+// onload
+$.win.on('load', function() {
+	$.http.get('/api/user/').on({
+		complete: function(data) {
+			append(data);
+		}
+	});
 });
+
+function append(data) {
+	var target = $('#list');
+	var fragment = $.fragment();
+	for (var key in data) {
+		fragment.append(
+			$.tag('li')
+				.data({ name: data[key].name, id: data[key].id })
+				.tap(
+					function() {
+						location.replace('/master/' + data[key].id)
+					}
+				)
+				.tag('p.name').text(data[key].name).gat()
+				.tag('p.message').text(data[key].message).gat()
+		);
+	}
+	target.append(fragment);
+}
 	
 })();
